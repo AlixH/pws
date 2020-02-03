07186cca2cf48c2d80cb52290f2bbfd4e848ff62
soufianeAourinmouche <soufiane.aourinmouche@symphony.com>
2020-02-03 17:34:32 +0100

    commit unstaged files

--- front-end/src/Routes/index.js
+++ front-end/src/Routes/index.js
@@ -5,6 +5,7 @@ import Home from '../Components/Home';
 
 // Import routing components
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginForm from '../Components/LoginForm/LoginForm';
 
 export default class Routes extends React.Component{
 
@@ -12,7 +13,7 @@ export default class Routes extends React.Component{
         return (
             <Router>
                 <Switch>
-                    <Route path="/" exact component={Home}/>
+                    <Route path="/" exact component={LoginForm}/>
                     <Route path="/*" exact render={()=> <div>No page found</div>}> </Route>
                 </Switch>
             </Router>
