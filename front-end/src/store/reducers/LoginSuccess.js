51b93275ec587d0b289134c32b68f46f2675e129
soufianeAourinmouche <soufiane.aourinmouche@symphony.com>
2020-02-10 16:03:56 +0100

    storing id and token in localStorage

--- front-end/src/store/reducers/LoginSuccess.js
+++ front-end/src/store/reducers/LoginSuccess.js
@@ -3,7 +3,7 @@ import {SET_LOGIN_SUCCESS} from "../actions/LoginSuccess";
 const defaultState = {};
 const homePageURL = "http://localhost:3000/home";  // warning, the port is hardcoded here ! it may sucks if front-end port changes !
 
-const loginErrorReducer = (state = defaultState, action) => {
+const loginSuccessReducer = (state = defaultState, action) => {
     if (action.type === SET_LOGIN_SUCCESS) {
         window.location.href=homePageURL;
         return {loginError : action.isLoginSuccess};
@@ -11,4 +11,4 @@ const loginErrorReducer = (state = defaultState, action) => {
         return state;
     }
 };
-export default loginErrorReducer;
+export default loginSuccessReducer;
