5e83389a3ccab34a225d758bebcf96c9dfd91698
Aourinmouche Soufiane <soufiane.aourinmouche@etu.unice.fr>
2020-02-13 09:02:16 +0100

    upload file locally in server (#24)

--- back-end/server/controller/FileController.js
+++ back-end/server/controller/FileController.js
@@ -15,7 +15,25 @@ let storage = new GridFsStorage({
     }
 });
 
+const localStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${file.originalname}`);
+    }
+});
+
 let upload = null;
+let localUpload = null;
+
+let localDestination = multer({storage: localStorage});
+
+function fileUpload(req, res, next) {
+    localDestination.single('file1')(req, res, next)
+    next();
+}
+
 
 storage.on('connection', (db) => {
     //Setting up upload for a single file
@@ -23,7 +41,16 @@ storage.on('connection', (db) => {
         storage: storage
     }).single('file1');
 
+    //localUpload = multer({storage: localStorage}).single('file1');
+
 });
+
+/*const upload = multer({storage});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.post('/fileUpload', upload.single('file'), (req, res) => {
+    res.send('file uploaded');
+});*/
+
 
 module.exports.uploadFile = (req, res) => {
     console.log("uploadFile");
@@ -36,6 +63,10 @@ module.exports.uploadFile = (req, res) => {
         console.log("req " + req.file);
         console.log("req " + req.file.filename);
         res.status(200).send({title: 'Uploaded', message: `File ${req.file.filename} has been uploaded!`});
+    });
+
+    fileUpload(req, res, () => {
+        console.log("dnas le next()");
     });
 };
 
