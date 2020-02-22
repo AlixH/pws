9db0b6586c82fb0febe43dcb50718ffccff784a2
AlixH <alix.humbert@etu.unice.fr>
2020-02-22 10:53:35 +0100

    improved navbar

--- front-end/src/Components/Navbar/Navbar.js
+++ front-end/src/Components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 // eslint-disable-file no-unused-vars
 import React from 'react';
 import "./NavBar.css";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import {useDispatch, useSelector} from "react-redux";
 import {SET_LOGIN_SUCCESS} from "../../store/actions/LoginSuccess";
 import shallowEqual from "react-redux/lib/utils/shallowEqual";
-import {Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 
 function NavBar(properties) {
@@ -34,15 +34,23 @@ function NavBar(properties) {
 
     }
 
+    function goHome() {
+        history.push('/home')
+    }
+
     // let plugin = new PluginModel(...properties.plugin);
     return (
         <div className={"nav-bar"}>
+            <h1 title={"Accueil"} onClick={() => goHome()} id={"site_title"}>AudiEffect</h1>
 
-            <Link to={"/home"}><h1 id={"site_title"}>Gallerie de plugins</h1></Link>
+
             {isLoggedIn ?
                 <div id={"buttons"}>
-                    <Button id={"upload_button"} size={"large"}  onClick={() => openUploadForm()} >Upload un plugin</Button>
-                    <Button id={"logout_button"} size={"large"}  onClick={() => logOut()}>Déconnexion</Button>
+                    {history.location.pathname === '/home' ?
+                        <Button id={"upload_button"} size={"large"} onClick={() => openUploadForm()}>Upload un plugin</Button>
+                        : null
+                    }
+                    <Button id={"logout_button"} size={"large"} onClick={() => logOut()}>Déconnexion</Button>
 
                 </div>
                 :
